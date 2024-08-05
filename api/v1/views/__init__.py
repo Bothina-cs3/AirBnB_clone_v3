f634733188cedb7efb947f8109b39abf156309b1
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-08-05 18:32:43 +0300

    add

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,17 +3,9 @@
  create a file __init__.py
 """
 
-#!/usr/bin/python3
-""" Blueprint for API """
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Importing after Blueprint creation to avoid circular imports
 from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places_amenities import *
