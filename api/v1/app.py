f02ccfeecf85d92d35a566a4b1b1edc913430417
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-08-05 18:14:23 +0300

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,21 +3,33 @@
 create a variable app, instance of Flask
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views
 from os import getenv
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
+
+
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", 5000)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
