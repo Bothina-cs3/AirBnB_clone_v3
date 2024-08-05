c956ebfbc9d4071de50a9c2ad1b8174dfe447dc1
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-08-05 18:44:11 +0300

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,17 @@ create a file index.py
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
     return jsonify({"status": "OK"})
 
