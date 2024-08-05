97805654fe0fb3e61ff12ce15eb4ee24eb76985d
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-08-05 21:00:39 +0300

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,7 +17,13 @@ def teardown_db(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Not found"}), 404
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
